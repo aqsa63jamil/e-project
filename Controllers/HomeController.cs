2d3582acf1bacc3f664174740562e5543ebe2e3e
asp <asp@aptechnn2.edu>
2024-01-26 15:33:30 +0500

    e-project upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using E_PROJECT.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_PROJECT.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
